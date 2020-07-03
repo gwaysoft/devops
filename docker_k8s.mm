55890d75b2a199bdad9d7988cff3d225c7485dea
gwaysoft <gwaysoft@outlook.com>
2020-07-03 14:15:49 +0800

    docker & k8s start

---
+++ docker_k8s.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1593756866707" ID="ID_152132444" MODIFIED="1593756883120" TEXT="docker &amp; k8s"/>
+</map>
