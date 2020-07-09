bb7829d4cab572272e16434a67bb0e462e849563
gwaysoft <gwaysoft@outlook.com>
2020-07-09 18:06:14 +0800

    docker

--- docker_k8s.mm
+++ docker_k8s.mm
@@ -1,4 +1,370 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1593756866707" ID="ID_152132444" MODIFIED="1593756883120" TEXT="docker &amp; k8s"/>
+<node CREATED="1593756866707" ID="ID_152132444" MODIFIED="1593756883120" TEXT="docker &amp; k8s">
+<node CREATED="1593757145152" ID="ID_75884518" MODIFIED="1593757154589" POSITION="right" TEXT="docker">
+<node CREATED="1594263203950" ID="ID_995758107" MODIFIED="1594263206036" TEXT="level">
+<node CREATED="1594263213560" ID="ID_293283256" MODIFIED="1594263214572" TEXT="start">
+<node CREATED="1594262130190" ID="ID_497588849" MODIFIED="1594263434756" TEXT="https://www.bilibili.com/video/BV1og4y1q7M4"/>
+<node CREATED="1594263636225" ID="ID_1558924427" MODIFIED="1594263651122" TEXT="docker introduction">
+<node CREATED="1594264929432" ID="ID_1655934955" MODIFIED="1594264934298" TEXT="devops">
+<node CREATED="1594263740929" ID="ID_1479112237" MODIFIED="1594264399387" TEXT="development as a operation"/>
+<node CREATED="1594268377746" ID="ID_275883321" MODIFIED="1594268407390" TEXT="&#x5e94;&#x7528;&#x66f4;&#x5feb;&#x901f;&#x7684;&#x4ea4;&#x4ed8;&#x548c;&#x90e8;&#x7f72;"/>
+<node CREATED="1594268440641" ID="ID_1700100935" MODIFIED="1594268472807" TEXT="&#x7528;docker&#xff0c;&#x751f;&#x4ea7;&#x4e00;&#x952e;&#x90e8;&#x7f72;&#x8fd0;&#x884c;"/>
+</node>
+<node CREATED="1594265017977" ID="ID_793635247" MODIFIED="1594265021838" TEXT="process">
+<node CREATED="1594264952034" ID="ID_1745084029" MODIFIED="1594264992693" TEXT="java -&gt; apk -&gt; app store -&gt; download -&gt; install - use"/>
+<node CREATED="1594265025705" ID="ID_94763456" MODIFIED="1594265084717" TEXT="java -&gt; image -&gt; docker store -&gt; download -&gt; run (container) -&gt; use"/>
+</node>
+<node CREATED="1594265357137" FOLDED="true" ID="ID_1361045044" MODIFIED="1594287619671" TEXT="history">
+<node CREATED="1594265373400" ID="ID_357901631" MODIFIED="1594265376310" TEXT="2010">
+<node CREATED="1594265418115" ID="ID_629527968" MODIFIED="1594265425062" TEXT="dotcloud">
+<node CREATED="1594265429579" ID="ID_225726714" MODIFIED="1594265448086" TEXT="paas"/>
+</node>
+</node>
+<node CREATED="1594265952113" ID="ID_532062270" MODIFIED="1594265956174" TEXT="2013">
+<node CREATED="1594265905113" ID="ID_1738440085" MODIFIED="1594265907875" TEXT="docker">
+<node CREATED="1594265907875" ID="ID_156521294" MODIFIED="1594265912838" TEXT="opensource"/>
+</node>
+</node>
+<node CREATED="1594266012001" ID="ID_867662669" MODIFIED="1594266030413" TEXT="2014.4.9">
+<node CREATED="1594266031474" ID="ID_627229365" MODIFIED="1594266038709" TEXT="docker 1.0 release"/>
+</node>
+<node CREATED="1594266291370" ID="ID_1941172612" MODIFIED="1594266302629" TEXT="vm vs docker">
+<node CREATED="1594268009200" ID="ID_1495882379" MODIFIED="1594268012629" TEXT="vm">
+<node CREATED="1594268165904" ID="ID_39988277" MODIFIED="1594268203749" TEXT="&#x539f;&#x751f;os &#xff08;kernel + lib + app&#xff09;"/>
+<node CREATED="1594268013441" ID="ID_1429304682" MODIFIED="1594268039014" TEXT="&#x8d44;&#x6e90;&#x5360;&#x7528;&#xff0c;&#x5197;&#x4f59;&#x591a;"/>
+<node CREATED="1594268022840" ID="ID_887621996" MODIFIED="1594268028814" TEXT="&#x5f00;&#x673a;&#x6162;"/>
+</node>
+<node CREATED="1594268040929" ID="ID_497215818" MODIFIED="1594268047950" TEXT="docker">
+<node CREATED="1594268050344" ID="ID_1848171870" MODIFIED="1594268244094" TEXT="&#x4e0d;&#x662f;&#x6a21;&#x62df;&#x4e86;&#x4e00;&#x4e2a;os &#xff08;lib + app&#xff09;&#xff0c;&#x5145;&#x5206;&#x5229;&#x7528;&#x5bbf;&#x4e3b;&#x673a;host os&#x7684;&#x8d44;&#x6e90;"/>
+<node CREATED="1594268253208" ID="ID_676167796" MODIFIED="1594272964108" TEXT="container&#x4e92;&#x76f8;&#x9694;&#x79bb;&#xff0c;&#x9694;&#x79bb;, docker&#x6838;&#x5fc3;&#x601d;&#x60f3;, &#x4e92;&#x76f8;&#x9694;&#x79bb;"/>
+<node CREATED="1594264415169" ID="ID_1455682391" MODIFIED="1594264424790" TEXT="cross platform"/>
+<node CREATED="1594263681072" ID="ID_366251882" MODIFIED="1594263730576" TEXT="environment development &lt;-&gt; test &lt;-&gt; uat &lt;-&gt; production">
+<node CREATED="1594268630440" ID="ID_1291633102" MODIFIED="1594268635206" TEXT="&#x73af;&#x5883;&#x9ad8;&#x5ea6;&#x4e00;&#x81f4;"/>
+</node>
+<node CREATED="1594263779665" ID="ID_1292706109" MODIFIED="1594263785053" TEXT="cluster">
+<node CREATED="1594263786280" ID="ID_339407980" MODIFIED="1594263797158" TEXT="redis, es, hadoop"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1593757155400" FOLDED="true" ID="ID_852976034" MODIFIED="1594287697039" TEXT="install">
+<node CREATED="1594267696057" FOLDED="true" ID="ID_1772600583" MODIFIED="1594287615512" TEXT="docker keywords">
+<node CREATED="1594273076758" ID="ID_1732150930" MODIFIED="1594273084923" TEXT="compose">
+<node CREATED="1594273089670" ID="ID_514880015" MODIFIED="1594273093139" TEXT="client"/>
+<node CREATED="1594273110382" ID="ID_207886901" MODIFIED="1594273128563" TEXT="docker host">
+<node CREATED="1594273129382" ID="ID_502679327" MODIFIED="1594273135995" TEXT="docker daemon"/>
+<node CREATED="1594273137184" ID="ID_1772448452" MODIFIED="1594273193147" TEXT="container">
+<node CREATED="1594273244318" ID="ID_351845295" MODIFIED="1594273252724" TEXT="docker start"/>
+<node CREATED="1594273254654" ID="ID_810450692" MODIFIED="1594273262539" TEXT="docker attach"/>
+<node CREATED="1594273337351" ID="ID_1017000615" MODIFIED="1594273346963" TEXT="like linux instance"/>
+</node>
+<node CREATED="1594273145606" ID="ID_1198133583" MODIFIED="1594273194731" TEXT="image">
+<node CREATED="1594273216631" ID="ID_1806826673" MODIFIED="1594273220323" TEXT="template"/>
+<node CREATED="1594273229511" ID="ID_1530058446" MODIFIED="1594273237923" TEXT="docker run"/>
+</node>
+</node>
+<node CREATED="1594273151374" ID="ID_1164951232" MODIFIED="1594273157092" TEXT="registry">
+<node CREATED="1594273157759" ID="ID_160287199" MODIFIED="1594273163732" TEXT="repository">
+<node CREATED="1594273355095" ID="ID_30478968" MODIFIED="1594273358643" TEXT="docker pull"/>
+<node CREATED="1594273385799" ID="ID_415613511" MODIFIED="1594273388395" TEXT="type">
+<node CREATED="1594273369278" ID="ID_941943090" MODIFIED="1594273375363" TEXT="public"/>
+<node CREATED="1594273376991" ID="ID_159187338" MODIFIED="1594273379195" TEXT="private"/>
+</node>
+</node>
+<node CREATED="1594273395950" ID="ID_26831068" MODIFIED="1594273413516" TEXT="&#x914d;&#x7f6e;&#x955c;&#x50cf;&#x52a0;&#x901f;"/>
+</node>
+</node>
+</node>
+<node CREATED="1593757159665" ID="ID_1593841041" MODIFIED="1593757163681" TEXT="centos">
+<node CREATED="1593757163681" ID="ID_1079662551" MODIFIED="1593757190604" TEXT="linux.mm -&gt; centos -&gt; service -&gt; docker"/>
+<node CREATED="1594275536527" ID="ID_470047687" MODIFIED="1594275559884" TEXT="aliyun &#x955c;&#x50cf;&#x52a0;&#x901f;">
+<node CREATED="1594275627439" ID="ID_576474562" MODIFIED="1594275651290" TEXT="&#x5bb9;&#x5668;&#x955c;&#x50cf;&#x670d;&#x52a1; -&gt; &#x955c;&#x50cf;&#x52a0;&#x901f;&#x5668;"/>
+<node CREATED="1594275568270" ID="ID_1259002817" MODIFIED="1594275598006">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      sudo mkdir -p /etc/docker
+    </p>
+    <p>
+      sudo tee /etc/docker/daemon.json &lt;&lt;-'EOF'
+    </p>
+    <p>
+      {
+    </p>
+    <p>
+      &#160;&#160;&quot;registry-mirrors&quot;: [&quot;https://5gvjxvqt.mirror.aliyuncs.com&quot;]
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      EOF
+    </p>
+    <p>
+      sudo systemctl daemon-reload
+    </p>
+    <p>
+      sudo systemctl restart docker
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1594278709231" ID="ID_760149465" MODIFIED="1594278712939" TEXT="docker info"/>
+</node>
+</node>
+<node CREATED="1593757259448" ID="ID_985961537" MODIFIED="1593757263948" TEXT="windows">
+<node CREATED="1593757265272" ID="ID_522187683" MODIFIED="1593757266421" TEXT="DockerToolbox-18.03.0-ce.exe"/>
+</node>
+</node>
+<node CREATED="1594263226672" ID="ID_1772577560" MODIFIED="1594263233103" TEXT="command">
+<node CREATED="1594277865247" FOLDED="true" HGAP="23" ID="ID_973607559" MODIFIED="1594287613318" TEXT="principle" VSHIFT="1">
+<node CREATED="1594277931559" ID="ID_202033380" MODIFIED="1594277932276" TEXT="cs">
+<node CREATED="1594277933431" ID="ID_1224340265" MODIFIED="1594277939612" TEXT="client"/>
+<node CREATED="1594277941023" ID="ID_399091568" MODIFIED="1594277947620" TEXT="docker daemon"/>
+<node CREATED="1594277962631" ID="ID_1973203280" MODIFIED="1594278006356" TEXT="client &lt;=&gt; socket &lt;=&gt; docker daemon"/>
+</node>
+<node CREATED="1594278181400" ID="ID_1090820567" MODIFIED="1594278321428" TEXT="why&#x6bd4;vm&#x5feb;">
+<node CREATED="1594278208879" ID="ID_1231990681" MODIFIED="1594278234334" TEXT="docker&#x6bd4;vm&#x6709;&#x66f4;&#x5c11;&#x7684;&#x62bd;&#x8c61;&#x5c42;"/>
+<node CREATED="1594278326240" ID="ID_64069204" MODIFIED="1594278386124" TEXT="run docker&#x65f6;&#x4e0d;&#x9700;&#x8981;&#x52a0;&#x8f7d;&#x4e00;&#x4e2a;os, docker&#x5229;&#x7528;&#x4e86;&#x5bbf;&#x4e3b;&#x673a;host&#x7684;kernel"/>
+</node>
+<node CREATED="1594277682455" ID="ID_445910939" MODIFIED="1594277694168" TEXT="docker run">
+<node CREATED="1594277694168" ID="ID_1637307703" MODIFIED="1594277759356" TEXT="start -&gt; search local image -&gt; search at docker hub -&gt; download -&gt; run"/>
+</node>
+</node>
+<node CREATED="1594278549200" ID="ID_664848668" MODIFIED="1594278555099" TEXT="command">
+<node CREATED="1594278556895" ID="ID_575454124" MODIFIED="1594289103414" TEXT="help">
+<node CREATED="1594278817119" ID="ID_740241178" MODIFIED="1594289107917" TEXT="https://docs.docker.com/engine/reference/commandline/docker/"/>
+<node CREATED="1594278561551" ID="ID_1912104289" MODIFIED="1594278567844" TEXT="docker version"/>
+<node CREATED="1594278569527" ID="ID_1467010750" MODIFIED="1594278572155" TEXT="docker info"/>
+<node CREATED="1594278573639" ID="ID_1537390627" MODIFIED="1594278586564" TEXT="docker --help"/>
+<node CREATED="1594278996350" ID="ID_651682779" MODIFIED="1594279001708" TEXT="docker run --help"/>
+</node>
+<node CREATED="1594279017559" ID="ID_480325369" MODIFIED="1594288761122" TEXT="image">
+<node CREATED="1594279046919" ID="ID_1250489734" MODIFIED="1594279050844" TEXT="docker images"/>
+<node CREATED="1594279365223" ID="ID_1413816831" MODIFIED="1594285780908" TEXT="docker search">
+<node CREATED="1594285781854" ID="ID_438948626" MODIFIED="1594285782659" TEXT="web">
+<node CREATED="1594279373008" ID="ID_1776614472" MODIFIED="1594279374564" TEXT="https://hub.docker.com/"/>
+</node>
+<node CREATED="1594286499104" ID="ID_772193013" MODIFIED="1594286502237" TEXT="docker search mysql -f=stars=3000"/>
+</node>
+<node CREATED="1594286508417" ID="ID_372046197" MODIFIED="1594286511949" TEXT="docker pull">
+<node CREATED="1594287269808" FOLDED="true" ID="ID_776118394" MODIFIED="1594287277276" TEXT="docker pull mysql">
+<node CREATED="1594286745153" ID="ID_539093570" MODIFIED="1594287220248">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      docker pull mysql
+    </p>
+    <p>
+      Using default tag: latest
+    </p>
+    <p>
+      latest: Pulling from library/mysql
+    </p>
+    <p>
+      8559a31e96f4: Pull complete
+    </p>
+    <p>
+      d51ce1c2e575: Pull complete
+    </p>
+    <p>
+      c2344adc4858: Pull complete
+    </p>
+    <p>
+      fcf3ceff18fc: Pull complete
+    </p>
+    <p>
+      16da0c38dc5b: Pull complete
+    </p>
+    <p>
+      b905d1797e97: Pull complete
+    </p>
+    <p>
+      4b50d1c6b05c: Pull complete
+    </p>
+    <p>
+      c75914a65ca2: Pull complete
+    </p>
+    <p>
+      1ae8042bdd09: Pull complete
+    </p>
+    <p>
+      453ac13c00a3: Pull complete
+    </p>
+    <p>
+      9e680cd72f08: Pull complete
+    </p>
+    <p>
+      a6b5dc864b6c: Pull complete
+    </p>
+    <p>
+      Digest: sha256:8b7b328a7ff6de46ef96bcf83af048cb00a1c86282bfca0cb119c84568b4caf6
+    </p>
+    <p>
+      Status: Downloaded newer image for mysql:latest
+    </p>
+    <p>
+      docker.io/library/mysql:latest
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1594287081328" FOLDED="true" ID="ID_218083601" MODIFIED="1594287293326" TEXT="docker pull mysql:5.7.30">
+<node CREATED="1594287286296" ID="ID_87874084" MODIFIED="1594287291382">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      docker pull mysql:5.7.30
+    </p>
+    <p>
+      5.7.30: Pulling from library/mysql
+    </p>
+    <p>
+      8559a31e96f4: Already exists
+    </p>
+    <p>
+      d51ce1c2e575: Already exists
+    </p>
+    <p>
+      c2344adc4858: Already exists
+    </p>
+    <p>
+      fcf3ceff18fc: Already exists
+    </p>
+    <p>
+      16da0c38dc5b: Already exists
+    </p>
+    <p>
+      b905d1797e97: Already exists
+    </p>
+    <p>
+      4b50d1c6b05c: Already exists
+    </p>
+    <p>
+      d85174a87144: Pull complete
+    </p>
+    <p>
+      a4ad33703fa8: Pull complete
+    </p>
+    <p>
+      f7a5433ce20d: Pull complete
+    </p>
+    <p>
+      3dcd2a278b4a: Pull complete
+    </p>
+    <p>
+      Digest: sha256:32f9d9a069f7a735e28fd44ea944d53c61f990ba71460c5c183e610854ca4854
+    </p>
+    <p>
+      Status: Downloaded newer image for mysql:5.7.30
+    </p>
+    <p>
+      docker.io/library/mysql:5.7.30
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1594287184641" ID="ID_1094321696" MODIFIED="1594287189025" TEXT="tags">
+<node CREATED="1594287189025" ID="ID_1996023124" MODIFIED="1594287190157" TEXT="https://hub.docker.com/_/mysql?tab=tags"/>
+</node>
+</node>
+<node CREATED="1594287321009" ID="ID_147316786" MODIFIED="1594287325013" TEXT="docker images"/>
+<node CREATED="1594287354289" ID="ID_1778801231" MODIFIED="1594287360590" TEXT="docker rmi">
+<node CREATED="1594287433729" ID="ID_1447432373" MODIFIED="1594287484475" TEXT="docker rmi -f $(docker images -aq)"/>
+</node>
+</node>
+<node CREATED="1594279021191" ID="ID_735536407" MODIFIED="1594279024044" TEXT="container">
+<node CREATED="1594287724233" ID="ID_497355912" MODIFIED="1594287730454" TEXT="docker run">
+<node CREATED="1594287795025" ID="ID_513468834" MODIFIED="1594287803902" TEXT="--name=&quot;Name&quot;"/>
+<node CREATED="1594287813049" ID="ID_925667057" MODIFIED="1594287815861" TEXT="-d">
+<node CREATED="1594287816673" ID="ID_1816415133" MODIFIED="1594287824766" TEXT="daemon"/>
+</node>
+<node CREATED="1594287827193" ID="ID_1622217249" MODIFIED="1594287830598" TEXT="-it">
+<node CREATED="1594287832121" ID="ID_1655449707" MODIFIED="1594287853902" TEXT="interactive"/>
+</node>
+<node CREATED="1594287865250" ID="ID_239032300" MODIFIED="1594287908786" TEXT="-p">
+<node CREATED="1594287908787" ID="ID_662416954" MODIFIED="1594287921308" TEXT="-p 8080:8080">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1594287969730" ID="ID_490918775" MODIFIED="1594287972526" TEXT="practice">
+<node CREATED="1594287973265" ID="ID_1372157525" MODIFIED="1594287996774" TEXT="docker run -it centos /bin/bash">
+<node CREATED="1594288062154" ID="ID_1083233962" MODIFIED="1594288071926" TEXT="&#x542f;&#x52a8;&#x5e76;&#x8fdb;&#x5165;&#x5bb9;&#x5668;"/>
+</node>
+</node>
+<node CREATED="1594288359522" ID="ID_898834388" MODIFIED="1594288377822" TEXT="exit container">
+<node CREATED="1594288378713" ID="ID_1812938895" MODIFIED="1594288379645" TEXT="exit">
+<node CREATED="1594288479849" ID="ID_527099151" MODIFIED="1594288486934" TEXT="stop container"/>
+</node>
+<node CREATED="1594288381168" ID="ID_776919578" MODIFIED="1594288397405" TEXT="ctrl + p +q">
+<node CREATED="1594288489305" ID="ID_759980730" MODIFIED="1594288496638" TEXT="not stop container"/>
+</node>
+</node>
+</node>
+<node CREATED="1594288189369" ID="ID_1130403463" MODIFIED="1594288281790" TEXT="docker ps -a -n=1"/>
+<node CREATED="1594288517760" ID="ID_1482435739" MODIFIED="1594288525085" TEXT="docker rm">
+<node CREATED="1594288766720" ID="ID_15542181" MODIFIED="1594288845944" TEXT="docker ps -aq | xargs docker rm -f"/>
+</node>
+<node CREATED="1594288921425" ID="ID_1381524025" MODIFIED="1594288952944">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      docker start | restart | stop | kill
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1594263234376" ID="ID_663097565" MODIFIED="1594263239876" TEXT="docker images"/>
+<node CREATED="1594263241143" ID="ID_861950279" MODIFIED="1594263253107" TEXT="container volume"/>
+<node CREATED="1594263254486" ID="ID_548303388" MODIFIED="1594263259059" TEXT="dockerfile"/>
+<node CREATED="1594263260455" ID="ID_698549090" MODIFIED="1594263265860" TEXT="docker network"/>
+<node CREATED="1594263268398" ID="ID_1188328389" MODIFIED="1594263276051" TEXT="idea integrate docker"/>
+</node>
+<node CREATED="1594263399005" ID="ID_326726494" MODIFIED="1594263400634" TEXT="up">
+<node CREATED="1594263279171" ID="ID_883926165" MODIFIED="1594263371107" TEXT="orchestration">
+<node CREATED="1594263372319" ID="ID_1069228466" MODIFIED="1594263381050" TEXT="docker compose"/>
+<node CREATED="1594263382318" ID="ID_2390363" MODIFIED="1594263387665" TEXT="docker swarm"/>
+<node CREATED="1594263389784" ID="ID_79339351" MODIFIED="1594263392706" TEXT="k8s"/>
+</node>
+<node CREATED="1594263404795" ID="ID_1042761343" MODIFIED="1594263414807" TEXT="ci\cd jenkins"/>
+</node>
+</node>
+<node CREATED="1594267489865" ID="ID_627732712" MODIFIED="1594267580173" TEXT="docker resource">
+<node CREATED="1594267581113" ID="ID_1827424565" MODIFIED="1594267582598" TEXT="hub">
+<node CREATED="1594267507624" ID="ID_604240467" MODIFIED="1594267518448" TEXT="account">
+<node CREATED="1594267518449" ID="ID_390026017" MODIFIED="1594267523173" TEXT="gwaysoft"/>
+<node CREATED="1594267524721" ID="ID_693154355" MODIFIED="1594267533440" TEXT="gwaysoft@outlook.com"/>
+<node CREATED="1594267534625" ID="ID_882861837" MODIFIED="1594267541141" TEXT="******S**"/>
+</node>
+</node>
+<node CREATED="1594267586416" ID="ID_601846534" MODIFIED="1594267589741" TEXT="docs">
+<node CREATED="1594267235505" ID="ID_453748448" MODIFIED="1594267240523" TEXT="https://docs.docker.com/">
+<node CREATED="1594267246113" ID="ID_1687822566" MODIFIED="1594267251943" TEXT="&#x975e;&#x5e38;&#x8be6;&#x7ec6;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
 </map>
