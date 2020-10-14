4d876225d487593d9ef7272864449f19b8d5712d
gwaysoft <gwaysoft@outlook.com>
2020-10-14 14:09:28 +0800

    kubernetes

---
+++ cicd.mm
@@ -0,0 +1,89 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1602213707329" ID="ID_1425011889" MODIFIED="1602213719800" TEXT="ci/cd">
+<node CREATED="1602214406719" ID="ID_994869673" MODIFIED="1602214431676" POSITION="right" TEXT="jenkins">
+<node CREATED="1602214989846" ID="ID_674597445" MODIFIED="1602214990977" TEXT="https://www.bilibili.com/video/BV1mt411u7An?p=30"/>
+<node CREATED="1602214798807" ID="ID_1553444703" MODIFIED="1602214808204" TEXT="introduce">
+<node CREATED="1602214809527" ID="ID_1207318126" MODIFIED="1602214813084" TEXT="overview">
+<node CREATED="1602214556279" FOLDED="true" ID="ID_845411373" MODIFIED="1602214698916" TEXT="&#x975e;&#x5bb9;&#x5668;&#x5316;&#x7684;">
+<node CREATED="1602214435575" ID="ID_890190261" MODIFIED="1602214446324">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="pictures/cicd/jenkins_overview.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1602214651327" FOLDED="true" ID="ID_695171852" MODIFIED="1602214688588" TEXT="&#x5bb9;&#x5668;&#x5316;">
+<node CREATED="1602214658671" ID="ID_1505537593" MODIFIED="1602214664067">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="pictures/cicd/jenkins_overview_container.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1602214844839" ID="ID_61640347" MODIFIED="1602223401608" TEXT="pipeline">
+<node CREATED="1602215389463" FOLDED="true" ID="ID_1972907676" MODIFIED="1602223386611" TEXT="overview">
+<node CREATED="1602215394033" ID="ID_1005796775" MODIFIED="1602215419046">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="pictures/cicd/jenkins_pipeline.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1602214865272" ID="ID_1308415153" MODIFIED="1602214877404" TEXT="cicd, best practice"/>
+<node CREATED="1602214923991" ID="ID_1699695530" MODIFIED="1602214929196" TEXT="&#x6d41;&#x6c34;&#x7ebf;"/>
+<node CREATED="1602215122479" ID="ID_472704546" MODIFIED="1602215129455" TEXT="stage">
+<node CREATED="1602215129456" ID="ID_765592729" MODIFIED="1602215143652" TEXT="&#x6309;&#x7167;&#x5de5;&#x4f5c;&#x6d41;"/>
+</node>
+<node CREATED="1602215290992" ID="ID_1876668763" MODIFIED="1602215297676" TEXT="Jenkinsfile">
+<node CREATED="1602215306519" ID="ID_1248846947" MODIFIED="1602215314019" TEXT="&#x53ef;&#x4ee5;&#x653e;&#x5230;git&#x4e2d;"/>
+</node>
+<node CREATED="1602223415655" FOLDED="true" ID="ID_1837918865" MODIFIED="1602223710699" TEXT="kubernetes">
+<node CREATED="1602223391470" ID="ID_531279530" MODIFIED="1602223398924">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="pictures/cicd/jenkins_pipeline_kubernetes.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1602223717095" ID="ID_957171695" MODIFIED="1602223729316" TEXT="deployment">
+<node CREATED="1602223734775" ID="ID_1756122998" MODIFIED="1602223738700" TEXT="slave - pod"/>
+</node>
+</node>
+</node>
+<node CREATED="1602213792455" ID="ID_363220882" MODIFIED="1602213795667" POSITION="left" TEXT="basic">
+<node CREATED="1602213796455" ID="ID_60619647" MODIFIED="1602213990812" TEXT="conception">
+<node CREATED="1602213806126" ID="ID_945125952" MODIFIED="1602213830820" TEXT="Continuous Integration">
+<node CREATED="1602213932367" ID="ID_1869255893" MODIFIED="1602213972388" TEXT="&#x4ee3;&#x7801;&#x5408;&#x5e76;&#x3001;&#x6784;&#x5efa;&#x3001;&#x90e8;&#x7f72;&#x3001;&#x6d4b;&#x8bd5;&#x90fd;&#x5728;&#x4e00;&#x8d77;&#xff0c;&#x4e0d;&#x65ad;&#x6267;&#x884c;&#x8fd9;&#x4e2a;&#x8fc7;&#x7a0b;"/>
+</node>
+<node CREATED="1602213833191" ID="ID_897382762" MODIFIED="1602213843315" TEXT="Continuous Deployment">
+<node CREATED="1602213909287" ID="ID_868198839" MODIFIED="1602213930380" TEXT="&#x90e8;&#x7f72;&#x5230;&#xff0c;&#x6d4b;&#x8bd5;&#xff0c;uat&#xff0c;&#x751f;&#x4ea7;&#x73af;&#x5883;"/>
+</node>
+<node CREATED="1602213845991" ID="ID_11138375" MODIFIED="1602213863436" TEXT="Continuous Delivery">
+<node CREATED="1602213899566" ID="ID_670401947" MODIFIED="1602213907932" TEXT="&#x53d1;&#x5e03;&#x5230;&#x751f;&#x4ea7;"/>
+</node>
+</node>
+<node CREATED="1602213992207" ID="ID_1937526942" MODIFIED="1602213995820" TEXT="goal">
+<node CREATED="1602213996799" ID="ID_1541816097" MODIFIED="1602214025556" TEXT="&#x81ea;&#x52a8;&#x6267;&#x884c;&#xff0c;&#x51cf;&#x5c11;&#x4eba;&#x4e3a;&#x5e72;&#x9884;"/>
+</node>
+</node>
+</node>
+</map>
