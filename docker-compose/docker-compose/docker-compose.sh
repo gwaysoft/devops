2055c4eb7530dff622ddfcf3098bf81fad1fe1c5
gwaysoft <gwaysoft@outlook.com>
2020-10-16 14:56:51 +0800

    kubernetes

--- docker-compose/docker-compose/docker-compose.sh
+++ docker-compose/docker-compose/docker-compose.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sleep 2
+chmod +x /usr/local/bin/docker-compose
+sleep 1
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sleep
 docker-compose --version
--- docker/docker-ce/centos7_docker-ce.sh
+++ docker/docker-ce/centos7_docker-ce.sh
@@ -13,6 +13,8 @@ sudo yum remove docker \
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 # Step 2: 添加软件源信息
 sudo yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sleep 10
+echo "sleep 10"
 # Step 3: 更新并安装Docker-CE
 sudo yum makecache fast
 sudo yum -y install docker-ce
